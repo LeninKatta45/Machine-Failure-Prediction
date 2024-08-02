899aa797ab6dc31e163eb137d6373bde11e77cfc
LeninKatta <108182396+LeninKatta45@users.noreply.github.com>
2024-08-02 10:20:03 +0000

    Update

--- model.py
+++ model.py
@@ -7,57 +7,64 @@ import numpy as np
 import sklearn
 from sklearn.ensemble import RandomForestClassifier
 import json
+
 # 2. Create the app object
 from fastapi.middleware.cors import CORSMiddleware
 
 # Add CORS middleware to allow requests from all origins
 
+
 class Smart(BaseModel):
-    	 		
+
     Type: int
     Air_temperature: float
     Process_temperature: float
-    Rotational_speed : float
+    Rotational_speed: float
     Torque_NM: float
-    Tool_wear_min:float
+    Tool_wear_min: float
+
 
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
 def Final():
-    with open("maint.pickle4","rb") as f:
-        classifier=pickle.load(f)
+    with open("maint.pickle4", "rb") as f:
+        classifier = pickle.load(f)
     return classifier
 
-@app.get('/')
+
+@app.get("/")
 def index():
-    return {'message': 'Hello, World'}
+    return {"message": "Hello, World"}
 
 
-@app.post('/predict')
+@app.post("/predict")
 def predict(data: Smart):
-    Type= data.Type
-    Air_temperature= data.Air_temperature
-    Process_temperature=data.Process_temperature
+    Type = data.Type
+    Air_temperature = data.Air_temperature
+    Process_temperature = data.Process_temperature
     Rotational_speed = data.Rotational_speed
-    Torque_NM= data.Torque_NM
-    Tool_wear_min=data.Tool_wear_min
-    x=[[ Type,
-    Air_temperature,
-    Process_temperature,
-    Rotational_speed,
-    Torque_NM,
-    Tool_wear_min]]
-    prediction =Final().predict(x)
-    if(prediction[0]==1):
-        prediction="Machine Can Fail"
+    Torque_NM = data.Torque_NM
+    Tool_wear_min = data.Tool_wear_min
+    x = [
+        [
+            Type,
+            Air_temperature,
+            Process_temperature,
+            Rotational_speed,
+            Torque_NM,
+            Tool_wear_min,
+        ]
+    ]
+    prediction = Final().predict(x)
+    if prediction[0] == 1:
+        prediction = "Machine Can Fail"
     else:
-        prediction="Machine Has Life"
-    return {
-        'prediction': prediction
-    }
+        prediction = "Machine Has Life"
+    return {"prediction": prediction}
